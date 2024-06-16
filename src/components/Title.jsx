a49784f17dffe72dc9e785b677761222b656dd67
Ogaaaa <baotogia0203@gmail.com>
2024-06-17 06:14:26 +0700

    nh6

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -6,7 +6,7 @@ const Title = () => {
       <div class="two">
         <h1>
           QUẢN LÝ VẬT TƯ
-          <span>by Nhom 46</span>
+          <span>by Nhom 6</span>
         </h1>
       </div>
     </>
